df5f4c80b130852254c2067731f7ad6ead284ad7
James Maddox <james.maddox07@gmail.com>
2020-04-08 21:49:27 -0400

    rendered list of countries with drop down

---
+++ src/api/index.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+// Url where the API is located
+const url = 'https://covid19.mathdro.id/api';
+
+// Here we are using async/await to fetch that data and storing it into variable 'data'
+// We then use destructuring to get out the data that we need: confirmed, recovered, etc.
+export const fetchData = async () => {
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
+
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {}
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+
+    return modifiedData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+
+    return countries.map((country) => country.name);
+  } catch (error) {
+    console.log(error);
+  }
+};
